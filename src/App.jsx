2eb17582604d27c6c65f4b8a7b83009ae90f71f1
PiyushMishra1114 <cse2118.diatm@gmail.com>
2025-03-29 10:58:01 +0530

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import TopBar from './TopBar/TopBar';
+import HeroSection from './HeroSection/HeroSection';
+import KeyPillars from './KeyPillars/KeyPillars';
+// import Footer from './Footer/Footer';
+
+function App() {
+    return (
+        <div>
+        <TopBar />
+        <HeroSection />
+        <KeyPillars />
+        {/* <Footer /> */}
+        </div>
+    );
+}
+
+export default App;
